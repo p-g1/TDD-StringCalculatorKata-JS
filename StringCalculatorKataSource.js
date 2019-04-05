f5edd95c0451288fc63c63157271d622f4a85d4b
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-05 12:37:24 +0100

    big solution refactor

--- StringCalculatorKataSource.js
+++ StringCalculatorKataSource.js
@@ -1,33 +1,10 @@
 exports.StringCalculator = numbers => {
-  if (numbers == "") {
-    return 0;
-  }
-
+  let total = 0;
   numbers = numbers.split(",");
 
-  if (numbers.length === 2) {
-    let first = numbers[0];
-    let last = numbers[1];
+  numbers.forEach(number => {
+    total += Number(number);
+  });
 
-    return Number(first) + Number(last);
-  }
-
-  if (numbers.length == 3) {
-    let first = numbers[0];
-    let second = numbers[1];
-    let last = numbers[2];
-
-    return Number(first) + Number(second) + Number(last);
-  }
-
-  if (numbers.length == 4) {
-    let first = numbers[0];
-    let second = numbers[1];
-    let third = numbers[2];
-    let last = numbers[3];
-
-    return Number(first) + Number(second) + Number(third) + Number(last);
-  }
-
-  return Number(numbers);
+  return total;
 };

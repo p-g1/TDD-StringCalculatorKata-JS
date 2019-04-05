aa1fdae1b6cb7358ef718e388b2823880cee62f3
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-05 09:18:53 +0100

    Added test for 1

--- StringCalculatorKataSource.js
+++ StringCalculatorKataSource.js
@@ -2,4 +2,8 @@ exports.StringCalculator = x => {
   if (x == "") {
     return 0;
   }
+
+  if (x == "1") {
+    return 1;
+  }
 };
--- StringCalculatorkataSource.test.js
+++ StringCalculatorkataSource.test.js
@@ -3,3 +3,7 @@ const Source = require("./StringCalculatorKataSource.js");
 test("should return zero when given empty string", () => {
   expect(Source.StringCalculator("")).toBe(0);
 });
+
+test("should return correct number when given a single value string", () => {
+  expect(Source.StringCalculator("1")).toBe(1);
+});

3fbd360f4dd1de4713c5f757b164bc91cf61ac60
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-05 11:42:01 +0100

    Added extra test for three value string

--- StringCalculatorkataSource.test.js
+++ StringCalculatorkataSource.test.js
@@ -16,6 +16,8 @@ test("should return correct number when given two value string", () => {
   expect(Source.StringCalculator("99,2")).toBe(101);
 });
 
-test("should return correct number when given '2,3,4'", () => {
+test("should return correct number when given three value string", () => {
   expect(Source.StringCalculator("2,3,4")).toBe(9);
+  expect(Source.StringCalculator("10,15,4")).toBe(29);
+  expect(Source.StringCalculator("10,10,10")).toBe(30);
 });

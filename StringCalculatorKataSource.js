d0a062c3f965dd9b98298e9df395f132b01a70eb
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-05 12:11:45 +0100

    refactored split

--- StringCalculatorKataSource.js
+++ StringCalculatorKataSource.js
@@ -3,26 +3,28 @@ exports.StringCalculator = numbers => {
     return 0;
   }
 
-  if (numbers.split(",").length === 2) {
-    let first = numbers.split(",")[0];
-    let last = numbers.split(",")[1];
+  numbers = numbers.split(",");
+
+  if (numbers.length === 2) {
+    let first = numbers[0];
+    let last = numbers[1];
 
     return Number(first) + Number(last);
   }
 
-  if (numbers.split(",").length == 3) {
-    let first = numbers.split(",")[0];
-    let second = numbers.split(",")[1];
-    let last = numbers.split(",")[2];
+  if (numbers.length == 3) {
+    let first = numbers[0];
+    let second = numbers[1];
+    let last = numbers[2];
 
     return Number(first) + Number(second) + Number(last);
   }
 
-  if (numbers.split(",").length == 4) {
-    let first = numbers.split(",")[0];
-    let second = numbers.split(",")[1];
-    let third = numbers.split(",")[2];
-    let last = numbers.split(",")[3];
+  if (numbers.length == 4) {
+    let first = numbers[0];
+    let second = numbers[1];
+    let third = numbers[2];
+    let last = numbers[3];
 
     return Number(first) + Number(second) + Number(third) + Number(last);
   }

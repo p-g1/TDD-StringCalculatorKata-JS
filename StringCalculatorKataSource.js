cf2aaf86214eebf489fef876bd930e06121d10ee
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-05 09:26:09 +0100

    Refactored conditional for single values

--- StringCalculatorKataSource.js
+++ StringCalculatorKataSource.js
@@ -3,15 +3,5 @@ exports.StringCalculator = x => {
     return 0;
   }
 
-  if (x == "1") {
-    return 1;
-  }
-
-  if (x == "2") {
-    return 2;
-  }
-
-  if (x == "99") {
-    return 99;
-  }
+  return +x;
 };

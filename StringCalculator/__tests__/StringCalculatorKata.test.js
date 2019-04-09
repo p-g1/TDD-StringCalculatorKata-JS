86521d2f690fb6f1b35feb03280bef04b2c35a9b
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-09 17:10:36 +0100

    test refactoring

--- StringCalculator/__tests__/StringCalculatorKata.test.js
+++ StringCalculator/__tests__/StringCalculatorKata.test.js
@@ -16,39 +16,70 @@ describe("should return correct number when given a single value string", () => 
   });
 });
 
-test("should return correct number when given two value string", () => {
-  expect(Source.StringCalculator("1,2")).toBe(3);
-  expect(Source.StringCalculator("3,2")).toBe(5);
-  expect(Source.StringCalculator("99,2")).toBe(101);
+describe("should return correct number when given two value string", () => {
+  test.each`
+    input     | expectedResult
+    ${"1,2"}  | ${3}
+    ${"3,2"}  | ${5}
+    ${"99,2"} | ${101}
+  `("$input converts to $expectedResult", ({ input, expectedResult }) => {
+    expect(Source.StringCalculator(input)).toBe(expectedResult);
+  });
 });
 
-test("should return correct number when given three value string", () => {
-  expect(Source.StringCalculator("2,3,4")).toBe(9);
-  expect(Source.StringCalculator("10,15,4")).toBe(29);
-  expect(Source.StringCalculator("10,10,10")).toBe(30);
+describe("should return correct number when given three value string", () => {
+  test.each`
+    input         | expectedResult
+    ${"2,3,4"}    | ${9}
+    ${"10,15,4"}  | ${29}
+    ${"10,10,10"} | ${30}
+  `("$input converts to $expectedResult", ({ input, expectedResult }) => {
+    expect(Source.StringCalculator(input)).toBe(expectedResult);
+  });
 });
 
-test("should return correct number when given four value string", () => {
-  expect(Source.StringCalculator("1,2,3,4")).toBe(10);
-  expect(Source.StringCalculator("3,4,5,6")).toBe(18);
+describe("should return correct number when given four value string", () => {
+  test.each`
+    input            | expectedResult
+    ${"1,2,3,4"}     | ${10}
+    ${"3,4,5,6"}     | ${18}
+    ${"10,10,10,10"} | ${40}
+  `("$input converts to $expectedResult", ({ input, expectedResult }) => {
+    expect(Source.StringCalculator(input)).toBe(expectedResult);
+  });
 });
 
-test("should return correct number when given single newline character in string", () => {
-  expect(Source.StringCalculator("1\n2,3")).toBe(6);
-  expect(Source.StringCalculator("1,2\n3")).toBe(6);
-  expect(Source.StringCalculator("1,200,100\n600")).toBe(901);
+describe("should return correct number when given single newline character in string", () => {
+  test.each`
+    input               | expectedResult
+    ${"1\n2,3"}         | ${6}
+    ${"1,2\n3"}         | ${6}
+    ${"1,200,100\n600"} | ${901}
+  `("input converts to $expectedResult", ({ input, expectedResult }) => {
+    expect(Source.StringCalculator(input)).toBe(expectedResult);
+  });
 });
 
-test("should return correct number when given multiple newline characters in string", () => {
-  expect(Source.StringCalculator("1\n2\n3")).toBe(6);
-  expect(Source.StringCalculator("12,2\n9\n1")).toBe(24);
-  expect(Source.StringCalculator("99\n200\n100\n600")).toBe(999);
+describe("should return correct number when given multiple newline characters in string", () => {
+  test.each`
+    input                  | expectedResult
+    ${"1\n2\n3"}           | ${6}
+    ${"12,2\n9\n1"}        | ${24}
+    ${"99\n200\n100\n600"} | ${999}
+  `("input converts to $expectedResult", ({ input, expectedResult }) => {
+    expect(Source.StringCalculator(input)).toBe(expectedResult);
+  });
 });
 
-test("should return six when given string including custom delimiter", () => {
-  expect(Source.StringCalculator("//[,]\n1,2,3")).toBe(6);
-  expect(Source.StringCalculator("//[;]\n1;2;3")).toBe(6);
-  expect(Source.StringCalculator("//[\n]\n1\n2\n3")).toBe(6);
+describe("should return six when given string including custom delimiter", () => {
+  test.each`
+    input                | expectedResult
+    ${"//[,]\n1,2,3"}    | ${6}
+    ${"//[;]\n1;2;3"}    | ${6}
+    ${"//[\n]\n1\n2\n3"} | ${6}
+  `("input converts to $expectedResult", ({ input, expectedResult }) => {
+    expect(Source.StringCalculator(input)).toBe(expectedResult);
+  });
 });
 
 test("should throw an exception that shows value when given single negative in string", () => {
@@ -72,22 +103,37 @@ test("should throw an exception that shows all negative values when given multip
   }).toThrow("negatives not allowed: -1111,-222,-55");
 });
 
-test("should ignore any numbers bigger than 1000", () => {
-  expect(Source.StringCalculator("1000,2000,2")).toBe(2);
-  expect(Source.StringCalculator("1000,2000,2")).toBe(2);
-  expect(Source.StringCalculator("900000")).toBe(0);
+describe("should ignore any numbers bigger than 1000", () => {
+  test.each`
+    input             | expectedResult
+    ${"1000,2000,2"}  | ${2}
+    ${"10001,2000,2"} | ${2}
+    ${"900000"}       | ${0}
+  `("input converts to $expectedResult", ({ input, expectedResult }) => {
+    expect(Source.StringCalculator(input)).toBe(expectedResult);
+  });
 });
 
-test("should allow custom delimiters of any length", () => {
-  expect(Source.StringCalculator("//[xxx]\n998xxx1")).toBe(999);
-  expect(Source.StringCalculator("//[bbbbb]\n998bbbbb1")).toBe(999);
-  expect(Source.StringCalculator("//[bb!!!bb]\n997bb!!!bb1bb!!!bb1")).toBe(999);
+describe("should allow custom delimiters of any length", () => {
+  test.each`
+    input                                 | expectedResult
+    ${"//[xxx]\n998xxx1"}                 | ${999}
+    ${"//[bbbbb]\n998bbbbb1"}             | ${999}
+    ${"//[bb!!!bb]\n997bb!!!bb1bb!!!bb1"} | ${999}
+  `("input converts to $expectedResult", ({ input, expectedResult }) => {
+    expect(Source.StringCalculator(input)).toBe(expectedResult);
+  });
 });
 
-test("should allow multiple custom delimiters", () => {
-  expect(Source.StringCalculator("//[z][q]\n10z11z12q13")).toBe(46);
-  expect(Source.StringCalculator("//[k][m][s]\n1m2k3s4")).toBe(10);
-  expect(Source.StringCalculator("//[k][m][n][d]\n20d20n20m10k10")).toBe(80);
+describe("should allow multiple custom delimiters", () => {
+  test.each`
+    input                               | expectedResult
+    ${"//[z][q]\n10z11z12q13"}          | ${46}
+    ${"//[k][m][s]\n1m2k3s4"}           | ${10}
+    ${"//[k][m][n][d]\n20d20n20m10k10"} | ${80}
+  `("input converts to $expectedResult", ({ input, expectedResult }) => {
+    expect(Source.StringCalculator(input)).toBe(expectedResult);
+  });
 });
 
 test("should allow multiple custom delimiters of any length", () => {
